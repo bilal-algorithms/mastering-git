7370477e4521b9cd20bd4f8ac75e94acd17d1076
Bilal <bilal.kanjelkheir@gmail.com>
2025-02-08 20:53:42 +0200

    add console log

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
-console.log("this is a test")
-let something = 'test';
-console.log(`this is a ${something}`);
-console.log('hey all');
-let a = 1 + 2;
-console.log(a)
-
+console.log('hello, world!');
